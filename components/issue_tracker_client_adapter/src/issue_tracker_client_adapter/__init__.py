84202179f868715e68bc1048dc5ae3be0e99d1a3
hayden-hs <hr2712@nyu.edu>
2026-04-01 18:16:10 -0400

    fix: defer env var check to adapter instantiation time

--- components/issue_tracker_client_adapter/src/issue_tracker_client_adapter/__init__.py
+++ components/issue_tracker_client_adapter/src/issue_tracker_client_adapter/__init__.py
@@ -10,15 +10,18 @@ import issue_tracker_client_api.client as _api
 
 from issue_tracker_client_adapter.adapter import ServiceClientAdapter
 
-_url = os.environ.get("ISSUE_TRACKER_SERVICE_URL")
-if not _url:
-    msg = (
-        "Environment variable ISSUE_TRACKER_SERVICE_URL is not set. "
-        "Example: export ISSUE_TRACKER_SERVICE_URL=http://localhost:8000"
-    )
-    raise ValueError(msg)
 
-_url_str: str = _url
-_api.register(lambda: ServiceClientAdapter(base_url=_url_str))
+def _make_adapter() -> ServiceClientAdapter:
+    url = os.environ.get("ISSUE_TRACKER_SERVICE_URL")
+    if not url:
+        msg = (
+            "Environment variable ISSUE_TRACKER_SERVICE_URL is not set. "
+            "Example: export ISSUE_TRACKER_SERVICE_URL=http://localhost:8000"
+        )
+        raise ValueError(msg)
+    return ServiceClientAdapter(base_url=url)
+
+
+_api.register(_make_adapter)
 
 __all__ = ["ServiceClientAdapter"]
